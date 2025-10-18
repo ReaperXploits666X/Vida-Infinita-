2dcbcec1654f1e73c5dff7fbcea972ed82fb5b26
Reaper Xploits <canaldegtamodsmods@gmail.com>
2025-10-17 21:29:31 -0300

    AimNoob.lua

--- AimNoob.lua
+++ AimNoob.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ReaperXploits666X/AimNoob.lua/main/AimCamposMain.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ReaperXploits666X/AimNoob/main/AimCamposMain.lua", true))()
